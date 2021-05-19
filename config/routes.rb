c90abb913d9537b78f443c34bc87be04eef88d36
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-19 20:18:36 +0200

    ex2 to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # A visitor can see all the restaurants
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  resources :restaurants, only: [:index, :new, :show, :create]
 end
